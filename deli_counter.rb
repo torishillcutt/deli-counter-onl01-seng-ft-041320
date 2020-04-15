2f860d56c8072a10cdccbc8993ad71c39a8089bd
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 20:05:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
 
 def line(katz_deli)
- if katz_deli.length > 0 ? "string" : "The line is currently empty."
+ if katz_deli.length > 0 ? "string" : puts "The line is currently empty."
  end
 end
