2c8c9a622a33bcd833ba385519430301199c64e1
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 21:31:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,12 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length > 0
-     new_array = []
+     new_array = ["The line is currently: "]
      katz_deli.each_with_index do |name, index|
      new_array << "#{index + 1}. #{name}"
    end
-     puts "The line is currently: " + new_array.join(" ")
+     puts new_array.join(" ")
   end
 end
 
-
-#"The line is currently #{katz_deli.length} #{name}"
+def take_a_number
