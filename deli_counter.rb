ede9bd6ec155f8980332b5fb48b6c06587392b82
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 21:17:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.length == 0
-    "The line is currently empty."
+    puts "The line is currently empty."
   elsif katz_deli.length > 0
-     "The line is currently #{katz_deli.length} #{name}"
+     puts "The line is currently #{katz_deli.length} #{name}"
   end
 end
