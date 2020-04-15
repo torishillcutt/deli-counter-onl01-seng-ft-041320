8a590f2f1b3db3e511a49aedaf2cc63257bc2e49
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 21:27:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(katz_deli)
   elsif katz_deli.length > 0
      new_array = ["The line is currently: "]
      katz_deli.each_with_index do |name, index|
-     new_array << "#{index}. #{name}"
-     end
+     new_array << "#{index + 1}. #{name}"
+   end
      puts new_array.join(" , ")
   end
 end
