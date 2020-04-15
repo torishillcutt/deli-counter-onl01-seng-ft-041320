0c4d9080c52461c1b2c7a1a8b5325e2984ab985f
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-15 21:50:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,10 +16,9 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  katz_deli.shift
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-  puts "Currently serving #{katz_deli[1]}."
+  puts "Currently serving #{katz_deli[0]}."
   end
 end
